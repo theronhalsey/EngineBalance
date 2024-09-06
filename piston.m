b4e66cb6347251d92c70ca918c760bea31a4755e
theronHalsey <34664651+theronhalsey@users.noreply.github.com>
2024-09-06 17:32:12 -0400

    still working out trig...

--- piston.m
+++ piston.m
@@ -1,12 +1,19 @@
+rpm = 4000;
+rps = rpm/60;
+p = 1/rps;
 head_m = 1.0; % mass of piston head in kg
 rod_l = .155575; % length of connecting rod in meters
 crank_l = .099060; % length of crank throw in meters
 
-crank_angle = @(t) mod(t,2*pi);
-crank_rod_angle = asin(sin(crank_angle(t)*crank_l/rod_l)); % calculate the angle between the crank arm and piston rod
-stroke_rod_angle = @(t) pi - crank_rod_angle(t) - crank_angle(t); % calculate the angle between the piston stroke and piston rod
-crank_x = @(t) crank_l * cos(crank_angle(t)); % calculate the x position of the crank arm end at time t
+ar = @(t) 2*pi*rps.*mod(t,p);
+ac = @(t) asin( (crank_l/rod_l) * sin(ar(t)) ); % calculate the angle between the crank arm and piston rod
+ah = @(t) pi - abs(ac(t)) - abs(ar(t)); % calculate the angle between the piston stroke and piston rod
 
+crank_x = @(t) crank_l .* cos(ar(t)); % calculate the x position of the crank arm end at time t
+head_y = @(t) rod_l * sin(ah(t)) ./ sin(ar(t)); % calculate the y position of the piston head
 
-t = 0:.001:2*pi;
-plot(t,crank_x(t))
+t = 0:.00001:p;
+hold on
+yline(rod_l+crank_l)
+yline(rod_l-crank_l)
+plot(t(2:end-1),head_y(t(2:end-1)))
