c6753ee4d79549ca5622e3b3303f5e1605a0dee6
theronHalsey <34664651+theronhalsey@users.noreply.github.com>
2024-09-08 15:02:29 -0400

    switched a calculation to law of cosines

--- piston.m
+++ piston.m
@@ -6,7 +6,7 @@ half_p = p/2;
 
 % time parameters
 dt = .000001; % time increment in seconds
-t = 0:dt:(3/4)*p;
+t = 0:dt:2*p;
 
 % piston head
 head_m = 1.0; % mass of piston head in kg
@@ -25,21 +25,22 @@ tdc_l = rod_l + crank_l;
 bdc_l = rod_l - crank_l;
 
 %% Function Handles
-crank_angle = @(t) 2*pi*rps.*mod(t,p);
-rod_angle = @(t) asin((crank_l/rod_l) * sin(crank_angle(t))); % calculate the angle between the crank arm and piston rod
-crank_rod_angle = @(t) pi - rod_angle(t) - crank_angle(t); % calculate the angle between the piston stroke and piston rod
-crank_xy = @(t) crank_l .* [cos(crank_angle(t)); sin(crank_angle(t))]; % calculate the [x;y] positions of the crank arm end at time t
-rod_com_xy = @(t) crank_l .* [0.5 * sin(crank_angle(t)); 0.5 * rod_l + cos(crank_angle(t))]; % calculate the [x;y] positions of com of connecting rod at time t
+f_crank_angle = @(t) 2*pi*rps.*mod(t,p);
+f_rod_angle = @(t) asin((crank_l/rod_l) * sin(f_crank_angle(t))); % calculate the angle between the crank arm and piston rod
+f_crank_rod_angle = @(t) pi - f_rod_angle(t) - f_crank_angle(t); % calculate the angle between the piston stroke and piston rod
+f_head_y = @(t) sqrt(crank_l^2 + rod_l^2 - 2*crank_l*rod_l * cos(f_crank_rod_angle(t)));
+f_crank_xy = @(t) crank_l .* [cos(f_crank_angle(t)); sin(f_crank_angle(t))]; % calculate the [x;y] positions of the crank arm end at time t
+f_rod_com_xy = @(t) crank_l .* [0.5 * sin(f_crank_angle(t)); 0.5 * rod_l + cos(f_crank_angle(t))]; % calculate the [x;y] positions of com of connecting rod at time t
 
 %% Calculation
 % piston head
-head_y = arrayfun(@(t) calc_head_y(t,crank_rod_angle,crank_angle,p,half_p,tdc_l,bdc_l,rod_l), t); % piston head location
+head_y = f_head_y(t); % piston head location
 head_v = diff(head_y)/dt; % piston head velocity
 head_a = diff(head_v)/dt; % piston head acceleration
 head_f = head_m*head_a; % piston head force
 
 % piston rod
-rod_xy = rod_com_xy(t);
+rod_xy = f_rod_com_xy(t);
 rod_v = [diff(rod_xy(1,:)); diff(rod_xy(2,:))]/dt;
 rod_a = [diff(rod_v(1,:)); diff(rod_v(2,:))]/dt;
 rod_f = rod_m*rod_a;
@@ -83,15 +84,3 @@ legend('boxoff')
 
 nexttile
 plot(rod_xy(1,1:(end-1)), rod_xy(2,1:(end-1)))
-
-%% Functions
-function y = calc_head_y(t,crank_rod_angle,crank_angle,p,half_p,tdc_l,bdc_l,rod_l) % calculate the y position of the piston head
-switch mod(t,p)
-    case 0
-        y = tdc_l;
-    case half_p
-        y = bdc_l;
-    otherwise
-        y = rod_l * sin(crank_rod_angle(t)) ./ sin(crank_angle(t));
-end
-end
