c78899ddde3887d12f542e9bd180da7a5a918d56
theronHalsey <34664651+theronhalsey@users.noreply.github.com>
2024-09-11 15:29:24 -0400

    stores multiple pistons now

--- engine.m
+++ engine.m
@@ -1,96 +1,54 @@
+%% Common Parameters
 % engine parameters
 rpm = 4000;
 rps = rpm/60;
 p = 1/rps;
+n_pistons = 4;
 
 % time parameters
 dt = .00001; % time increment in seconds
 t = 0:dt:p+dt;
+n_points = length(t)-2;
 
 % crankshaft (assume crankshaft is balanced)
 crank_l = 0.099060/2; % length of crank throw in meters (3.9' stroke)
 f_crank_angle = @(t) 2*pi*rps.*mod(t,p);
 crank_angles = f_crank_angle(t);
+crank_xy = crank_l/4 .* [cos(crank_angles); sin(crank_angles)]; % track the motion of a single point of the crank shaft for refrence
 
 % piston parameters
 head_m = 1.0; % mass of piston head in kg
-piston_angle = 0;
-crank_offset = 0;
-
-% counterweight
-counterweight_m = 0.5; % mass of counterweight
-counterweight_l = crank_l * 0.75; % distance to center of mass of counterweight from center of crankshaft
-counterweight_offset = crank_offset + pi;
 
 % piston rod
 rod_m = 5.44311; %mass of piston rod in kg (12 lbs)
 rod_l = 0.1525; % length of connecting rod in meters (6' rod)
 rod_w = 0.0251; % width of connecting rod in meters
 
-Forces = piston(dt,crank_angles,crank_offset,crank_l,head_m,piston_angle,rod_m,rod_l,counterweight_m,counterweight_l,counterweight_offset);
+%% Individual Parameters
+% piston parameters
+piston_angle = 0:pi/4:2*pi;
+crank_offset = piston_angle + pi/2;
 
-crank_xy = Forces(1:2,:);
-head_xy = Forces(3:4,:);
-head_f = Forces(5:6,:);
-rod_xy = Forces(7:8,:);
-rod_f = Forces(9:10,:);
-counterweight_xy = Forces(11:12,:);
-counterweight_f = Forces(13:14,:);
+% counterweight
+counterweight_m = 0.5 * ones(1,n_pistons); % mass of counterweight
+counterweight_l = crank_l * 0.75 * ones(1,n_pistons); % distance to center of mass of counterweight from center of crankshaft
+counterweight_offset = crank_offset + pi;
+
+%% Calculate Piston Forces
+Forces = zeros(14,n_points,n_pistons);
+
+parfor i=1:n_pistons
+    Forces(:,:,i) = piston(dt,crank_angles,crank_offset(i),crank_l,head_m,piston_angle(i),rod_m,rod_l,counterweight_m(i),counterweight_l(i),counterweight_offset(i));
+end
+
+piston_crank_xy = Forces(1:2,:,2);
+head_xy = Forces(3:4,:,2);
+head_f = Forces(5:6,:,2);
+rod_xy = Forces(7:8,:,2);
+rod_f = Forces(9:10,:,2);
+counterweight_xy = Forces(11:12,:,2);
+counterweight_f = Forces(13:14,:,2);
 total_f = head_f + rod_f + counterweight_f;
-
-% %% Plotting
-% tiledlayout(2,2);
-% 
-% % piston head
-% nexttile
-% hold on
-% title("Piston Head")
-% % yyaxis left
-% % plot(t,head_xy(1,:),Color='b',LineStyle='-')
-% % plot(t,head_xy(2,:),Color='g',LineStyle='-')
-% % yyaxis right
-% plot(t,head_f(1,:),Color='r',LineStyle='-')
-% plot(t,head_f(2,:),Color='m',LineStyle='-')
-% %legend("x-displacement","y-displacement","x-force","y-force",Location='southoutside')
-% legend("x-force","y-force",Location='southoutside')
-% 
-% % piston rod
-% nexttile
-% hold on
-% title("Piston Rod")
-% % yyaxis left
-% % plot(t,rod_xy(1,:),Color='b',LineStyle='-')
-% % plot(t,rod_xy(2,:),Color='g',LineStyle='-')
-% % yyaxis right
-% plot(t,rod_f(1,:),Color='r',LineStyle='-')
-% plot(t,rod_f(2,:),Color='m',LineStyle='-')
-% %legend("x-displacement","y-displacement","x-force","y-force",Location='southoutside')
-% legend("x-force","y-force",Location='southoutside')
-% 
-% % plot counterweight
-% nexttile
-% hold on
-% title("Counterweight")
-% % yyaxis left
-% % plot(t,counterweight_xy(1,:),Color='b',LineStyle='-')
-% % plot(t,counterweight_xy(2,:),Color='g',LineStyle='-')
-% % yyaxis right
-% plot(t,counterweight_f(1,:),Color='r',LineStyle='-')
-% plot(t,counterweight_f(2,:),Color='m',LineStyle='-')
-% %legend("x-displacement","y-displacement","x-force","y-force",Location='southoutside')
-% legend("x-force","y-force",Location='southoutside')
-% 
-% % plot total force on crankshaft
-% nexttile
-% hold on
-% title("Total Force")
-% % yyaxis left
-% % plot(t,crank_angles,Color='b',LineStyle='-')
-% % yyaxis right
-% plot(t,total_f(1,:),Color='r',LineStyle='-')
-% plot(t,total_f(2,:),Color='m',LineStyle='-')
-% %legend("crank angle","x-force","y-force",Location='southoutside')
-% legend("x-force","y-force",Location='southoutside')
 
 %% Animation
 piston_head_color = 'r';
@@ -110,12 +68,13 @@ counterweight_path = animatedline('color',counterweight_color,'LineStyle','-');
 counterweight_body = animatedline('color',counterweight_color,'LineStyle','-');
 crank_arm_location = animatedline('color',crank_arm_color,'Marker','.','markersize',20);
 crank_arm_path = animatedline('color',crank_arm_color,'LineStyle','-');
-crank_arm_body = animatedline('color',crank_arm_color,'LineStyle','-');
-crank_shaft_location = animatedline('color',crank_shaft_color,'Marker','.','markersize',20);
-addpoints(crank_shaft_location,0,0)
+crank_rod_connection = animatedline('color',crank_arm_color,'LineStyle','-');
+crank_shaft_center = animatedline('color',crank_shaft_color,'Marker','.','markersize',20);
+crank_shaft_point = animatedline('color',crank_shaft_color,'Marker','.','markersize',20);
+crank_shaft_rotation = animatedline('color',crank_shaft_color,'LineStyle','-');
+addpoints(crank_shaft_center,0,0)
 
 i = 1;
-j = length(Forces);
 while 1
     clearpoints(piston_head_location)
     clearpoints(rod_com_location)
@@ -124,21 +83,24 @@ while 1
 
     clearpoints(rod_body)
     clearpoints(counterweight_body)
-    clearpoints(crank_arm_body)
+    clearpoints(crank_rod_connection)
+    clearpoints(crank_shaft_point)
 
     addpoints(piston_head_location,head_xy(1,i),head_xy(2,i))
     addpoints(piston_head_path,head_xy(1,i),head_xy(2,i))
-    addpoints(rod_body,[head_xy(1,i) crank_xy(1,i)],[head_xy(2,i) crank_xy(2,i)])
-    addpoints(crank_arm_location,crank_xy(1,i),crank_xy(2,i))
-    addpoints(crank_arm_path,crank_xy(1,i),crank_xy(2,i))
-    addpoints(crank_arm_body,[crank_xy(1,i) 0],[crank_xy(2,i) 0])
+    addpoints(rod_body,[head_xy(1,i) piston_crank_xy(1,i)],[head_xy(2,i) piston_crank_xy(2,i)])
+    addpoints(crank_arm_location,piston_crank_xy(1,i),piston_crank_xy(2,i))
+    addpoints(crank_arm_path,piston_crank_xy(1,i),piston_crank_xy(2,i))
+    addpoints(crank_rod_connection,[piston_crank_xy(1,i) 0],[piston_crank_xy(2,i) 0])
     addpoints(rod_com_location,rod_xy(1,i),rod_xy(2,i))
     addpoints(rod_com_path,rod_xy(1,i),rod_xy(2,i))
     addpoints(counterweight_location,counterweight_xy(1,i),counterweight_xy(2,i))
     addpoints(counterweight_path,counterweight_xy(1,i),counterweight_xy(2,i))
     addpoints(counterweight_body,[counterweight_xy(1,i) 0],[counterweight_xy(2,i) 0])
+    addpoints(crank_shaft_rotation,crank_xy(1,i),crank_xy(2,i))
+    addpoints(crank_shaft_point,[crank_xy(1,i) 0],[crank_xy(2,i) 0])
     
     drawnow
-    i = mod(i,j);
+    i = mod(i,n_points);
     i = i+1;
 end
--- piston.m
+++ piston.m
@@ -6,7 +6,7 @@ aR = offset_crank_angles - piston_angle; % angles opposite piston rod
 aC = asin((crank_l/rod_l) * sin(aR)); % angles opposite the crank arm
 aS = pi - aR - aC; % angles opposite the piston stroke
 head_displacement = sqrt(crank_l^2 + rod_l^2 - 2*crank_l*rod_l * cos(aS));
-crank_xy = crank_l .* [cos(offset_crank_angles); sin(offset_crank_angles)]; % calculate the [x;y] positions of the crank arm end at time t
+offset_crank_xy = crank_l .* [cos(offset_crank_angles); sin(offset_crank_angles)]; % calculate the [x;y] positions of the crank arm end at time t
 
 %% Calculate Forces
 % piston head
@@ -16,7 +16,7 @@ head_a = [diff(head_v(1,:)); diff(head_v(2,:))]/dt; % piston head acceleration
 head_f = head_m*head_a; % piston head force
 
 % piston rod
-rod_xy = (head_xy+crank_xy)/2;
+rod_xy = (head_xy+offset_crank_xy)/2;
 rod_v = [diff(rod_xy(1,:)); diff(rod_xy(2,:))]/dt;
 rod_a = [diff(rod_v(1,:)); diff(rod_v(2,:))]/dt;
 rod_f = rod_m*rod_a;
@@ -28,5 +28,5 @@ counterweight_a = [diff(counterweight_v(1,:)); diff(counterweight_v(2,:))]/dt;
 counterweight_f = counterweight_m*counterweight_a;
 
 %% Generate Output Matrix
-pistonForces = [crank_xy(:,1:end-2); head_xy(:,1:end-2); [head_f(:,end-1:end) head_f(:,1:end-2)]; rod_xy(:,1:end-2); [rod_f(:,end-1:end) rod_f(:,1:end-2)]; counterweight_xy(:,1:end-2); [counterweight_f(:,end-1:end) counterweight_f(:,1:end-2)]];
+pistonForces = [offset_crank_xy(:,1:end-2); head_xy(:,1:end-2); [head_f(:,end-1:end) head_f(:,1:end-2)]; rod_xy(:,1:end-2); [rod_f(:,end-1:end) rod_f(:,1:end-2)]; counterweight_xy(:,1:end-2); [counterweight_f(:,end-1:end) counterweight_f(:,1:end-2)]];
 end
